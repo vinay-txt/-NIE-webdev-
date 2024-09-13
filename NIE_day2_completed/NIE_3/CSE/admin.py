87637984e7122fde5b0e86e060e73b33108a38f3
vinay-txt <2023cs_vinayirabasappahugar_d@nie.ac.in>
2024-09-13 15:04:15 +0530

    Add files via upload

---
+++ NIE_day2_completed/NIE_3/CSE/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from CSE.models  import Student
+
+admin.site.register(Student)
---
+++ NIE_day2_completed/NIE_3/CSE/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    department = models.CharField(max_length=30)
+    college_name = models.CharField(max_length=30)
+    rollnumber = models.IntegerField()
+    def __str__(self):
+        return self.name
